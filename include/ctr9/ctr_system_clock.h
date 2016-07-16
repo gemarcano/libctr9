1f8b5dc89794a79fec7fdeaa9d69808788cc7cde
Gabriel Marcano <gabemarcano@yahoo.com>
2016-07-16 17:21:59 -0400

    Added ctr_system_clock documentation

--- include/ctr9/ctr_system_clock.h
+++ include/ctr9/ctr_system_clock.h
@@ -18,22 +18,52 @@
 extern "C" {
 #endif
 
+/**	@brief Represents the clock internal data.
+ */
 typedef struct
 {
 	uint64_t count;
 	ctr_timer timer;
 } ctr_system_clock;
 
-void ctr_system_clock_initialize(ctr_system_clock *clock, ctr_timer timer);
-
+/**	@brief Represents a detailed time structure used by the system clock.
+ */
 typedef struct
 {
 	int64_t seconds;
 	int32_t  nanoseconds;
 } ctr_clock_time;
 
+/**	@brief Initializes a clock, taking control of the specified timer.
+ *
+ *	Once initialized, the system clock overrides whatever settings the
+ *	timer was set to. In order for the clock to work, global IRQs must
+ *	be enabled.
+ *
+ *	@param[out] clock Pointer to clock data structure to initialize.
+ *	@param[in[ timer Timer to take control over.
+ *
+ */
+void ctr_system_clock_initialize(ctr_system_clock *clock, ctr_timer timer);
+
+/**	@brief Returns the number of milliseconds that have passed since the clock
+ *		started.
+ *
+ *	@param[in] clock Pointer to the clock to query.
+ *
+ *	@returns The number of milliseconds that have passed since the clock
+ *		started.
+ */
 uint64_t ctr_system_clock_get_ms(ctr_system_clock *clock);
 
+/**	@brief Returns a detailed time structure representing the amount of time
+ *		that has passed since the clock was started.
+ *
+ *	@param[in] clock Pointer to the clock to query.
+ *
+ *	@returns A detailed time structure representing the amount of time that
+ *		has passed since the clock was started.
+ */
 ctr_clock_time ctr_system_clock_get_time(ctr_system_clock *clock);
 
 #ifdef __cplusplus
