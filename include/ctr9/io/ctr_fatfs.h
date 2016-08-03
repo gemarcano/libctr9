a41fab90dd86d6fe545371ac07198de0841da3ab
Gabriel Marcano <gabemarcano@yahoo.com>
2016-08-02 23:10:55 -0400

    Added convenience function for initializing IO
    -Internal IO components can now be initialized separately from SD, using
    separate function calls (instead of having to call ctr_fatfs_initialize
    twice).

--- include/ctr9/io/ctr_fatfs.h
+++ include/ctr9/io/ctr_fatfs.h
@@ -40,12 +40,59 @@ extern "C" {
  *
  *	@post All structures passed in are initialized and the fatfs subsystem has
  *		been prepared for usage.
+ *
+ *	@returns 0 on success, anything else on failure.
  */
 int ctr_fatfs_initialize(
 	ctr_nand_interface *nand_io,
 	ctr_nand_crypto_interface *ctr_io,
 	ctr_nand_crypto_interface *twl_io,
 	ctr_sd_interface *sd_io);
+
+/**	@brief Initializes the IO subsystem for the NAND internal disks.
+ *
+ *	This is a helper function that takes in pointers to all necessary io
+ *	interfaces to hook up to the fatfs layer for the internal NAND "disks,"
+ *	consisting of TWLN, TWLP, and CTRNAND. This function also prepares fatfs for
+ *	usage.
+ *
+ *	@param[in,out] nand_io NAND io interface to initialize. May be used
+ *		independetly after initialization so long as it is not being
+ *		simultaneously used by fatfs.
+ *	@param[in,out] ctr_io CTRNAND io crypto interface to initialize. May be used
+ *		independetly after initialization so long as it is not being
+ *		simultaneously used by fatfs.
+ *	@param[in,out] twl_io TWL io crypto interface to initialize. May be used
+ *		independetly after initialization so long as it is not being
+ *		simultaneously used by fatfs.
+ *
+ *	@post All structures passed in are initialized and the fatfs subsystem has
+ *		been prepared for usage.
+ *
+ *	@returns 0 on success, anything else on failure.
+ */
+int ctr_fatfs_internal_initialize(
+	ctr_nand_interface *nand_io,
+	ctr_nand_crypto_interface *ctr_io,
+	ctr_nand_crypto_interface *twl_io);
+
+/**	@brief Initializes the IO subsystem for the SD card.
+ *
+ *	This is a helper function that takes in pointers to all necessary io
+ *	interfaces to hook up to the fatfs layer for the SD card.
+ *
+ *	@param[in,out] sd_io SD card io interface to initialize. May be used
+ *		independetly after initialization so long as it is not being
+ *		simultaneously used by fatfs.
+ *
+ *	@post All structures passed in are initialized and the fatfs subsystem has
+ *		been prepared for usage.
+ *
+ *	@returns 0 on success, anything else on failure. Returns a failure if the SD
+ *		card was not inserted.
+ */
+int ctr_fatfs_sd_initialize(ctr_sd_interface *sd_io);
+
 
 #ifdef __cplusplus
 }
--- src/ctr_fatfs.c
+++ src/ctr_fatfs.c
@@ -26,3 +26,20 @@ int ctr_fatfs_initialize(
 	return result;
 }
 
+int ctr_fatfs_internal_initialize(
+	ctr_nand_interface *nand_io,
+	ctr_nand_crypto_interface *ctr_io,
+	ctr_nand_crypto_interface *twl_io)
+{
+	int result = ctr_disks_initialize(nand_io, ctr_io, twl_io, NULL);
+	ctr_fatfs_default_setup(ctr_io, twl_io, NULL);
+	return result;
+}
+
+int ctr_fatfs_sd_initialize(ctr_sd_interface *sd_io)
+{
+	int result = ctr_disks_initialize(NULL, NULL, NULL, sd_io);
+	ctr_fatfs_default_setup(NULL, NULL, sd_io);
+	return result;
+}
+
