406de40964574e2b142b4cd2ce98618385b74d05
Gabriel Marcano <gabemarcano@yahoo.com>
2016-12-29 23:21:00 -0500

    libctr_core PXI initialization done by default
    -ctr_libctr9_init now sets the PXI base register address, now required
    by libctr_core, by default.

--- src/ctr_init.c
+++ src/ctr_init.c
@@ -2,6 +2,7 @@
 #include <ctr9/io/ctr_drives.h>
 #include <ctr9/ctr_screen.h>
 #include <ctr9/ctr_freetype.h>
+#include <ctr_core/ctr_core_pxi.h>
 
 typedef struct draw_s
 {
@@ -14,6 +15,7 @@ void __attribute__((weak)) ctr_libctr9_init(void);
 
 void __attribute__((weak)) ctr_libctr9_init(void)
 {
+	ctr_core_pxi_change_base((volatile uint32_t*)0x10008000);
 	draw_s *cakehax_fbs = (draw_s*)0x23FFFE00;
 	ctr_screen_initialize(&ctr_screen_top, cakehax_fbs->top_left, 400, 240, CTR_GFX_PIXEL_RGB8);
 	ctr_screen_initialize(&ctr_screen_bottom, cakehax_fbs->sub, 320, 240, CTR_GFX_PIXEL_RGB8);
