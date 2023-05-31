379a36728da10c46999979c465e7e2f8624058ae
Gabriel Marcano <gabemarcano@yahoo.com>
2023-05-31 16:43:40 -0700

    init: get screen framebuffers from PXI
     - Only the ARM11 can do proper screen init and screen management, so
       wait for two pointers to be pushed over PXI from the ARM11 to get the
       pointers for the screen framebuffers

--- src/ctr_init.c
+++ src/ctr_init.c
@@ -3,6 +3,7 @@
 #include <ctr9/ctr_screen.h>
 #include <ctr9/ctr_freetype.h>
 #include <ctr9/ctr_pxi.h>
+#include <ctr9/ctr_interrupt.h>
 
 typedef struct draw_s
 {
@@ -16,9 +17,13 @@ void __attribute__((weak)) ctr_libctr9_init(void);
 void __attribute__((weak)) ctr_libctr9_init(void)
 {
 	ctr_pxi_initialize();
-	draw_s *cakehax_fbs = (draw_s*)0x23FFFE00;
-	ctr_screen_initialize(&ctr_screen_top, cakehax_fbs->top_left, 400, 240, CTR_GFX_PIXEL_RGB8);
-	ctr_screen_initialize(&ctr_screen_bottom, cakehax_fbs->sub, 320, 240, CTR_GFX_PIXEL_RGB8);
+	// Pointers to the framebuffers for the top and bottom screens
+	void* top, *bottom;
+	// Get them from ARM11 via PXI
+	while (!ctr_pxi_pop(&top));
+	while (!ctr_pxi_pop(&bottom));
+	ctr_screen_initialize(&ctr_screen_top, top, 400, 240, CTR_GFX_PIXEL_RGB8);
+	ctr_screen_initialize(&ctr_screen_bottom, bottom, 320, 240, CTR_GFX_PIXEL_RGB8);
 	ctr_console_initialize(&ctr_screen_bottom);
 
 	ctr_freetype_initialize();
